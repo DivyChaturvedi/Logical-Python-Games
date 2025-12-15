491c76ff5d41d3fd57e75d9cf537a3a733e5ef82
Divy Chaturvedi <divychaturvedi990@gmail.com>
2025-12-15 16:54:24 +0530

    me

---
+++ new.py
@@ -0,0 +1,18 @@
+# price = 10
+# price = 20
+# print(price)
+
+
+# '''Age Generator'''
+# birth_year= int(input('Birth year: '))
+# age = 2025 - birth_year
+# print("Your age is: ",age)
+
+
+
+'''Weight'''
+# weight_lbs = input("weight in lbs: ")
+# weight_kg = int(weight_lbs) * 0.45
+# print(weight_kg)
+
+
