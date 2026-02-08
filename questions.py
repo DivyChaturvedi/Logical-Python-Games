5936e50f322065f732579ac17fe050a076fbb1d4
Divy Chaturvedi <divychaturvedi990@gmail.com>
2026-02-08 23:10:18 +0530

    new

---
+++ questions.py
@@ -0,0 +1,26 @@
+'''Integer to roman program'''
+# num=1248
+# ans=0
+# rem=0
+# power=1
+# def roman(ans):
+#     if (ans==8):
+#         return 'M'
+#     elif (ans==40):
+#         return 'CC'
+#     elif (ans==200):
+#         return 'XL'
+#     elif(ans==1000):
+#         return 'VIII'
+# s=""    
+# while(num):
+#     rem=num%10
+#     ans=rem*power
+#     power*=10
+#     num//=10
+#     s=s+roman(ans)
+# print(s)
+
+
+
+
